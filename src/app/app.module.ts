9dab84da444a26585d56a76e6bd39bd17bff3efc
Ambrose Ahurra <ndahura12@gmail.com>
2020-10-03 01:16:03 +0300

    Small code refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,9 @@ import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import {RouterModule, Routes} from '@angular/router';
-const appRoutes: Routes[{'path': 'products', 'component': ProductListComponet}];
+const appRoutes: Routes = [{path: 'products', component: ProductListComponent},
+{path: 'product:id', component: ProductPageComponent},
+{path: '', redirectTo: 'products', pathMatch: 'full'}];
 
 @NgModule({
   declarations: [
