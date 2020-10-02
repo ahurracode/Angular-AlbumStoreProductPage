839a26d5ffe7f404a0ac18918df89a40bbb755f0
Ambrose Ahurra <ndahura12@gmail.com>
2020-10-03 01:07:17 +0300

    Adds navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import {RouterModule, Routes} from '@angular/router';
+const appRoutes: Routes[{'path': 'products', 'component': ProductListComponet}];
 
 @NgModule({
   declarations: [
@@ -21,7 +23,8 @@ import { ProductListComponent } from './product-list/product-list.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
